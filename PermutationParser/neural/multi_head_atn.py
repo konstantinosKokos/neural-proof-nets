401a372e2c207ab1bf95cbf93bb85f320a2a6202
konstantinosKokos <konstantinos@riseup.net>
2020-05-12 20:00:50 +0200

    less python

--- PermutationParser/neural/multi_head_atn.py
+++ PermutationParser/neural/multi_head_atn.py
@@ -2,47 +2,42 @@ from typing import *
 
 import torch
 from torch import Tensor, LongTensor
-from torch.nn import Module, ModuleList, Linear, Dropout
-from torch.nn import functional as F
+from torch.nn import Module, Linear, Dropout
 
 
 def multihead_attn_fn(queries: Tensor, keys: Tensor, values: Tensor,
-                      qts: ModuleList, kts: ModuleList, vts: ModuleList,
                       mask: Optional[LongTensor] = None) -> Tensor:
-    qs = [qt(queries) for qt in qts]
-    ks = [kt(keys) for kt in kts]
-    vs = [vt(values) for vt in vts]
-    outputs = [scaled_dot_product(qs[i], ks[i], vs[i], mask) for i in range(len(qs))]
-    return torch.cat(outputs, dim=-1)
+    return mh_scaled_dot_product(queries, keys, values, mask)
 
 
-def scaled_dot_product(queries: Tensor, keys: Tensor, values: Tensor,
-                       mask: Optional[LongTensor] = None) -> Tensor:
-    dk = keys.shape[-1]
-    dividend = torch.tensor(dk, device=queries.device, dtype=torch.float)
+def mh_scaled_dot_product(queries: Tensor, keys: Tensor, values: Tensor,
+                          mask: Optional[LongTensor] = None) -> Tensor:
+    dk, num_heads = keys.shape[-2:]
+    dividend = torch.sqrt(torch.tensor(dk, device=queries.device, dtype=torch.float))
 
-    weights = torch.bmm(queries, keys.transpose(2, 1)) / torch.sqrt(dividend)  # [B, M, N]
+    weights = torch.einsum('bidh,bodh->bioh', queries, keys) / dividend
     if mask is not None:
+        mask = mask.unsqueeze(-1).repeat(1, 1, 1, num_heads)
         weights = weights.masked_fill_(mask == 0, value=-1e10)
-    weights = F.softmax(weights, dim=-1)
-    return torch.bmm(weights, values)
+    weights = weights.softmax(dim=-2)
+    return torch.einsum('bioh,bodh->bidh', weights, values).flatten(-2)
 
 
 class MultiHeadAttention(Module):
     def __init__(self, num_heads: int, d_q_in: int, d_k_in: int, d_v_in: int,
                  d_atn: int, d_v: int, d_out: int, dropout_rate: float = 0.1) -> None:
         super(MultiHeadAttention, self).__init__()
-        self.q_transformations = ModuleList([Linear(in_features=d_q_in, out_features=d_atn, bias=False)
-                                             for _ in range(num_heads)])
-        self.k_transformations = ModuleList([Linear(in_features=d_k_in, out_features=d_atn, bias=False)
-                                             for _ in range(num_heads)])
-        self.v_transformations = ModuleList([Linear(in_features=d_v_in, out_features=d_v, bias=False)
-                                             for _ in range(num_heads)])
+        self.num_heads = num_heads
+        self.q_transformation = Linear(in_features=d_q_in, out_features=d_atn*num_heads, bias=False)
+        self.k_transformation = Linear(in_features=d_k_in, out_features=d_atn*num_heads, bias=False)
+        self.v_transformation = Linear(in_features=d_v_in, out_features=d_v*num_heads, bias=False)
         self.wo = Linear(in_features=num_heads * d_v, out_features=d_out, bias=False)
         self.dropout = Dropout(dropout_rate)
 
     def forward(self, queries: Tensor, keys: Tensor, values: Tensor, mask: Optional[LongTensor] = None) -> Tensor:
-        mha = multihead_attn_fn(queries, keys, values, self.q_transformations, self.k_transformations,
-                                self.v_transformations, mask)
+        qs = self.q_transformation(queries).view(queries.shape[0], queries.shape[1], -1, self.num_heads)
+        ks = self.k_transformation(keys).view(keys.shape[0], keys.shape[1], -1, self.num_heads)
+        vs = self.v_transformation(values).view(values.shape[0], values.shape[1], -1, self.num_heads)
+        mha = multihead_attn_fn(qs, ks, vs, mask)
         mha = self.dropout(mha)
         return self.wo(mha)
