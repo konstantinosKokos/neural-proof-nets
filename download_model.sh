3036b312fa5e4da05967e714ffef1cdeef6ef2aa
konstantinosKokos <konstantinos@riseup.net>
2020-07-04 11:41:38 +0200

    eh

--- download_model.sh
+++ download_model.sh
@@ -2,4 +2,5 @@
 
 curl -o './stored_models/model_weights.tar.xz' 'https://surfdrive.surf.nl/files/index.php/s/EuUqRp3VYLBmoBk/download'
 tar -xf './stored_models/model_weights.tar.xz'
+mv './290.model' './stored_models/model_weights.p'
 rm './stored_models/model_weights.tar.xz'
