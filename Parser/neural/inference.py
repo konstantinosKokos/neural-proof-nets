f10a47b893e9e76537efa3c93b7c2540b3a248e0
konstantinosKokos <konstantinos@riseup.net>
2021-04-13 12:51:22 +0200

    variable weight path

--- Parser/neural/inference.py
+++ Parser/neural/inference.py
@@ -1,13 +1,12 @@
 from ..train import init_without_datasets, load_model, Parser
-_weights_path = './stored_models/model_weights.model'
 
 
-def get_model(device: str) -> Parser:
+def get_model(device: str, weights_path: str = './stored_models/model_weights.model') -> Parser:
     print('Initializing model...')
     model = init_without_datasets(device=device)
     print('Initialized.')
 
     print('Loading pre-trained parameters...')
-    load_model(model, load=_weights_path, map_location=device)
+    load_model(model, load=weights_path, map_location=device)
     print('Loaded.')
     return model
